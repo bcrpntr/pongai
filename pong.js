b0a4abc90cdc5a9dcc87954954a2f0303814a844
bcrpntr <44307215+bcrpntr@users.noreply.github.com>
2023-06-04 22:16:50 -0500

    Create pong.js

---
+++ pong.js
@@ -0,0 +1,7 @@
+  let canvas = document.getElementById('pongCanvas');
+let ctx = canvas.getContext('2d');
+
+let aiLevel = document.getElementById('aiLevel');
+let aiMax = document.getElementById('aiMax');
+
+// Pong game and AI code will go here
